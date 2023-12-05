26bba5eb9f897a4608f741dae45a674008234bf3
Uday Reddy <107260122+udayreddy-07@users.noreply.github.com>
2023-12-05 07:11:30 +0000

    Datatypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3      
+    + 3)           // code readability should be high
+
+console.log("Uday")
+
+
+let name = "Uday"  //string
+let age = 20  //number
+let isLoggedIn = false   //boolean
+
+//number => 2 to power 5
+//bigint
+//string => ""
+//boolean => true/false   yes/no
+//null => standalone value
+//undefined => value is not assigned
+//symbol => unique names
+
+
+//object
+
+console.log(typeof "Uday")
+console.log(typeof undefined)  //undefined
+console.log(typeof null)   //object
